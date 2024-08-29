52fd6cfbfa0a08db89297ccaa23a9ff08ac9ee59
trishitchar <trishitchar@gmail.com>
2024-08-29 15:28:25 +0530

    trying to write code in ts

---
+++ backend/dist/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import { urlencoded } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+const app = express();
+dotenv.config();
+// MIDDLEWARE
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use(urlencoded({ extended: true }));
+const corsAllowOrigin = {
+    origin: ['http://localhost:5173', 'https://ai-flix.onrender.com', 'https://aiflix-tc.vercel.app'],
+    credentials: true
+};
+app.use(cors(corsAllowOrigin));
+app.get('/', (req, res) => {
+    res.send('Hello, TypeScript with Node.js!');
+});
+app.listen(`${process.env.PORT}`, () => {
+    console.log(`server is running on port ${process.env.PORT}`);
+});
---
+++ backend/src/custom.declaration.ts
@@ -0,0 +1,2 @@
+declare module 'cookie-parser';
+    
