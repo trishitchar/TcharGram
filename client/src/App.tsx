7a77c239a050ba3affafb432863e5151be26055c
trishitchar <trishitchar@gmail.com>
2024-09-01 08:42:08 +0530

    shadcn setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
 
   return (
     <div>
       <h1 className='bg-red-400 font-bold'> lol hi</h1>
+      <Button>lol shadcn</Button>
     </div>
   )
 }
