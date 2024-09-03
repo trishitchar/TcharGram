c4d88e239bd2eaa33efcd04fe035af0c070db589
trishitchar <trishitchar@gmail.com>
2024-09-04 02:26:53 +0530

    build error fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ import Landing from './pages/Landing';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
 import NotFound from './pages/NotFound';
-import { useAuthMiddleware } from './middleware/useAuthMiddleware';
+// import { useAuthMiddleware } from './middleware/useAuthMiddleware';
 import Feed from './pages/Feed';
 
 const App: React.FC = () => {
