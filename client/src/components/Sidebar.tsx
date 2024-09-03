9b0a8ba7e4872425860f92019f20b51b79fd1f6f
trishitchar <trishitchar@gmail.com>
2024-09-03 23:43:55 +0530

    frontend start

---
+++ client/src/components/Sidebar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Sidebar: React.FC = () =>{
+    return <div>
+    sidebar
+    </div>
+}
+
+export default Sidebar;
--- client/src/pages/Feed.tsx
+++ client/src/pages/Feed.tsx
@@ -1,4 +1,6 @@
 import Header from '@/components/common/Header';
+import Sidebar from '@/components/Sidebar';
+// import { Sidebar } from 'lucide-react';
 import React from 'react';
 
 const Feed: React.FC = () => {
@@ -7,6 +9,8 @@ const Feed: React.FC = () => {
     <div className="feed-container">
       <Header/>
       welcome to insta feed
+      {/* <Sidebar/> */}
+      <Sidebar/>
     </div>
   );
 };
