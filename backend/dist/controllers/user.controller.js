f9380312d21cb5fd86486fe501ffb0ad2a060b6a
trishitchar <trishitchar@gmail.com>
2024-09-04 04:30:36 +0530

    samesite change

--- backend/dist/controllers/user.controller.js
+++ backend/dist/controllers/user.controller.js
@@ -78,7 +78,7 @@ export const login = async (req, res) => {
         };
         res.cookie('token', token, {
             httpOnly: false,
-            sameSite: 'none',
+            sameSite: 'lax',
             maxAge: 24 * 60 * 60 * 1000,
         });
         return res.json({
