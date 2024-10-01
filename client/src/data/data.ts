7820e63aac062a82979b7181c813c5dc123a9da5
trishitchar <trishitchar@gmail.com>
2024-10-01 21:48:09 +0530

    some readme change

--- client/src/data/data.ts
+++ client/src/data/data.ts
@@ -1,5 +1,5 @@
-export const userBaseURL = "http://localhost:8080/api/user";
-// export const userBaseURL = "https://social-media-422a.onrender.com/api/user";
+// export const userBaseURL = "http://localhost:8080/api/user";
+export const userBaseURL = "https://social-media-422a.onrender.com/api/user";
 
-export const postBaseURL = "http://localhost:8080/api/post"
-// export const postBaseURL = "https://social-media-422a.onrender.com/api/post"
+// export const postBaseURL = "http://localhost:8080/api/post"
+export const postBaseURL = "https://social-media-422a.onrender.com/api/post"
