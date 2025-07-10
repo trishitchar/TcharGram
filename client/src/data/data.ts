3add23bac29a8544cd4eceacc1dd35782f925f19
TRISHIT CHAR <90543153+trishitchar@users.noreply.github.com>
2025-07-10 14:51:29 +0530

    Update data.ts

--- client/src/data/data.ts
+++ client/src/data/data.ts
@@ -1,6 +1,6 @@
 // export const baseURL = "http://localhost:8080";
-// export const baseURL = "https://social-media-422a.onrender.com";
-export const baseURL = "https://tchargram.onrender.com";
+export const baseURL = "https://social-media-422a.onrender.com";
+// export const baseURL = "https://tchargram.onrender.com";
 
 export const userBaseURL = `${baseURL}/api/user`;
 
